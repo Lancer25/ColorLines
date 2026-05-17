5c545a960f613e59fe0f371d5fe581a5e0f7ee05
Lancer25 <Lancer25@users.noreply.github.com>
2026-05-17 20:07:59 +0800

    chore: scaffold dotnet solution

---
+++ src/ColorLines.Windows/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ColorLines.Windows;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
