f118fdcbbd32fcd56b213843af8fc8cced15b38f
Lancer25 <Lancer25@users.noreply.github.com>
2026-05-17 21:06:05 +0800

    feat: bind playable wpf board

--- src/ColorLines.Windows/MainWindow.xaml.cs
+++ src/ColorLines.Windows/MainWindow.xaml.cs
@@ -1,7 +1,5 @@
 using System.Windows;
-using ColorLines.Core.Board;
-using ColorLines.Core.Game;
-using ColorLines.Core.Rules;
+using ColorLines.Windows.ViewModels;
 
 namespace ColorLines.Windows;
 
@@ -10,42 +8,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
-        var engine = new GameEngine(new SystemRandomSource());
-        var state = engine.NewGame();
-        DataContext = new MainWindowPreviewModel(state);
-    }
-}
-
-public sealed class MainWindowPreviewModel
-{
-    public MainWindowPreviewModel(GameState state)
-    {
-        Score = state.Score;
-        NextPiecesText = string.Join(", ", state.NextPieces);
-        PreviewCells = state.Board.Cells()
-            .Select(ToCellText)
-            .ToArray();
-    }
-
-    public int Score { get; }
-
-    public string NextPiecesText { get; }
-
-    public IReadOnlyList<string> PreviewCells { get; }
-
-    private static string ToCellText(Cell cell)
-    {
-        return cell.Piece switch
-        {
-            PieceKind.Orange => "O",
-            PieceKind.Gray => "G",
-            PieceKind.Tuxedo => "T",
-            PieceKind.Calico => "C",
-            PieceKind.Black => "B",
-            PieceKind.White => "W",
-            PieceKind.BlueGray => "N",
-            _ => string.Empty
-        };
+        DataContext = GameViewModel.CreateForNewGame();
     }
 }
